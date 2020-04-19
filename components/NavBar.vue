8a8b1b5584eb88886d461db29e9897bd5136504c
AliciadW <alicia.dewet@living-stratus.com>
2020-04-19 11:47:54 +0200

    Lesson 2 - 1

---
+++ components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="nav">
+      <nuxt-link to="/" class="brand">
+        Real World Events
+      </nuxt-link>
+      <nuxt-link to="/">
+        List
+      </nuxt-link>
+      |
+      <nuxt-link to="/create">
+        Create
+      </nuxt-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NavBar"
+    }
+</script>
+
+<style scoped>
+
+</style>
