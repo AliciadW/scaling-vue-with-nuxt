927026382507a2d13549db88911050eef44938d4
AliciadW <alicia.dewet@living-stratus.com>
2020-04-19 13:39:39 +0200

    Lesson 6 - 3

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,41 @@
 <template>
   <div>
     <h1>Events</h1>
+
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
 
 <script>
+  import EventCard from "~/components/EventCard";
 
   export default {
     name: "index",
+    components: {
+      EventCard
+    },
     head() {
       return {
         title: 'Event Listings'
       }
     },
-    asyncData({ $axios }) {
-      return $axios.get('http://localhost:3001/events').then(response => {
+    asyncData({ $axios, error }) {
+      return $axios
+        .get('http://localhost:3001/events')
+        .then(response => {
         return {
           events: response.data
         }
+      }).catch(e => {
+        error({
+          statusCode: 503,
+          message: 'Unable to fetch events at this time. Please try again later.'
+        })
       })
     }
   }
